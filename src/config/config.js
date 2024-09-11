b723e509cf8eed2a41c5586e2f3c1b823cf14f85
KristofB <babkri@gmail.com>
2024-09-11 16:37:44 +0200

    create rate limiters against brute force

--- src/config/config.js
+++ src/config/config.js
@@ -17,5 +17,10 @@ module.exports = {
         secret: envVars.JWT_SECRET,
         accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
         refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS
+    },
+    rateLimiter: {
+        maxAttemptsPerDay: envVars.MAX_ATTEMPTS_PER_DAY,
+        maxAttemptsByIpUsername: envVars.MAX_ATTEMPTS_BY_IP_USERNAME,
+        maxAttemptsPerEmail: envVars.MAX_ATTEMPTS_PER_EMAIL
     }
 }
